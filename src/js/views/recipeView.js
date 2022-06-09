f03f9552be1a7a34b85024846ddb5f4923b138ce
LC64 <leecaudill@gmail.com>
2022-06-09 16:39:24 -0600

    fixed fraction error

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,7 +1,8 @@
 import View from './View.js';
 
 import icons from 'url:../../img/icons.svg';
-import { Fraction } from 'fractional';
+// import { Fraction } from 'fractional';
+import { numberToFraction } from '../helpers.js';
 
 class RecipeView extends View {
   // giving each of the 'views' this #parentElement will make it easy to render the spinner, render success or error messages, or to render the recipe itself.
@@ -130,7 +131,7 @@ class RecipeView extends View {
           <use href="${icons}#icon-check"></use>
         </svg>
         <div class="recipe__quantity">${
-          ing.quantity ? new Fraction(ing.quantity).toString() : '' // conditional here to make it empty if no number is present.
+          ing.quantity ? new numberToFraction(ing.quantity).toString() : '' // conditional here to make it empty if no number is present.
         } </div>
         <div class="recipe__description">
           <span class="recipe__unit">${ing.unit}</span>
